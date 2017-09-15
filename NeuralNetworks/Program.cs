9db67613105146e7230595e7758130f5d8919249
garsidestephen <garsidestephen@yahoo.com>
2017-09-15 05:53:13 +0100

    v3 branch commits

--- NeuralNetworks/Program.cs
+++ NeuralNetworks/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using NeuralNetworks.Entities;
+using NeuralNetworks.Data;
+using NeuralNetworks.Data.Abstractions;
 using NeuralNetworks.Logic;
 using NeuralNetworks.Logic.Abstractions;
 using NeuralNetworks.Logic.ActivationFunctions;
@@ -11,7 +12,7 @@ namespace NeuralNetworks
         static void Main(string[] args)
         {
             //Create the Network
-            // Weights
+            // Initiate Weights
             double[] hiddenLayerWeights = new double[] { 0.38286575972236, 0.441739972420847, 0.00519391335788831, 0.152334728814817 };
             double[] hiddenLayerBias = new double[] { 0.79060369394282, 0.25095684418965 };
             double[] outputLayerWeights = new double[] { 0.100294556049767, 0.24866028653861 };
@@ -22,24 +23,53 @@ namespace NeuralNetworks
             var neuralNetwork = neuralNetworkFactory.Create(2, 1, 2, hiddenLayerWeights, outputLayerWeights, hiddenLayerBias, outputLayerBias);
 
             // Train the Network
-            double[] data1 = { 0, 0 };
-            double[] data2 = { 0, 1 };
-            double[] data3 = { 1, 0 };
-            double[] data4 = { 1, 1 };
-
-            double[][] trainingData = { data1, data2, data3, data4 };
-
-            double[] result1 = { 0 };
-            double[] result2 = { 1 };
-            double[] result3 = { 1 };
-            double[] result4 = { 0 };
-            double[][] expectedResults = { result1, result2, result3, result4 };
+            //ToDo: Inject
+            ITrainingDataRepo trainingDataRepo = new TrainingDataRepo();
+            var trainingData = trainingDataRepo.GetTrainingDataXOR();
 
             //ToDo: Inject
             INeuralNetworkTrainingService trainingService = new NeuralNetworkTrainingService(new NeuralNetworkProcessingService());
-            trainingService.Train(neuralNetwork, trainingData, expectedResults, 2000, new Sigmoid(), 1);
+            double[][] trainingResults = trainingService.Train(neuralNetwork, trainingData, 4000, new Sigmoid(), 1);
+
+            OutputResultsToConsole(trainingResults);
+            Console.WriteLine("Training Complete ---------------");
+
+            // Test the Trained Network
+            //ToDo: Inject
+            INeuralNetworkProcessingService processingService = new NeuralNetworkProcessingService();
+            double[] testInputs1 = { 0, 0 };
+            double[][] testResults1 = { testInputs1, processingService.ProcessFeedForwardNeuralNetwork(neuralNetwork, testInputs1, new Sigmoid()) };
+            Console.WriteLine("Test1 Results (0 XOR 0) ---------------");
+            OutputResultsToConsole(testResults1);
+
+            double[] testInputs2 = { 1, 0 };
+            double[][] testResults2 = { testInputs2, processingService.ProcessFeedForwardNeuralNetwork(neuralNetwork, testInputs2, new Sigmoid()) };
+            Console.WriteLine("Test2 Results (1 XOR 0) ---------------");
+            OutputResultsToConsole(testResults2);
+
+            double[] testInputs3 = { 0, 1 };
+            double[][] testResults3 = { testInputs3, processingService.ProcessFeedForwardNeuralNetwork(neuralNetwork, testInputs3, new Sigmoid()) };
+            Console.WriteLine("Test3 Results (0 XOR 1) ---------------");
+            OutputResultsToConsole(testResults3);
+
+            double[] testInputs4 = { 1, 1 };
+            double[][] testResults4 = { testInputs4, processingService.ProcessFeedForwardNeuralNetwork(neuralNetwork, testInputs4, new Sigmoid()) };
+            Console.WriteLine("Test4 Results (1 XOR 1) ---------------");
+            OutputResultsToConsole(testResults4);
 
             Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Output Results To Console
+        /// </summary>
+        /// <param name="results">Results</param>
+        private static void OutputResultsToConsole(double[][] results)
+        {
+            for (int i = 0; i < results.Length; i++)
+            {
+                Console.WriteLine(string.Join(", ", results[i]));
+            }
         }
     }
 }
